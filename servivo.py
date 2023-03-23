4124176d1d54862756d55aec449d7daa3ebdb7e8
Lucas Senos Coutinho <56167804+LucasSenosC@users.noreply.github.com>
2023-03-23 16:04:45 -0300

    Update servivo.py

--- servivo.py
+++ servivo.py
@@ -1,5 +1,27 @@
 class SerVivo:  
     def __init__(self, pontos_de_vida, pontos_de_ataque):
-        self.pontos_de_vida = pontos_de_vida
-        self.pontos_de_ataque = pontos_de_ataque
-        
+        # Atributo extra.
+        # pontos_de_vida_máxima é a vida máxima que esse ser vivo pode ter
+        self.pontos_de_vida_maxima: int = pontos_de_vida
+        self.pontos_de_vida: int = pontos_de_vida
+        self.pontos_de_ataque: int = pontos_de_ataque
+
+    # Metodo extra.
+    # O ser vivo pode achar uma cura e se curar um determinado valor
+    # A cura é limitada aos pontos de vida máxima
+    def cura(self, valor_da_cura):
+        if self.pontos_de_vida + valor_da_cura <= self.pontos_de_vida_maxima:
+            self.pontos_de_vida += valor_da_cura
+        else:
+            self.pontos_de_vida = self.pontos_de_vida_maxima
+    
+    # Metodo extra.
+    # O ser vivo pode achar um elixir de vida ou ataque
+    # O elixir de vida aumenda os pontos de vida máxima e completa a vida restante
+    # O elixir de ataque aumenta os pontos de ataque do ser vivo
+    def elixir(self, tipo, valor):
+        if tipo == "vida":
+            self.pontos_de_vida_maxima+= valor
+            self.pontos_de_vida = self.pontos_de_vida_maxima
+        if tipo == "ataque":
+            self.pontos_de_ataque += valor
