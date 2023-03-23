a99061a2eb3fe6a03c90dc65cc82c62babb4fa1f
Lucas Senos Coutinho <56167804+LucasSenosC@users.noreply.github.com>
2023-03-23 16:05:18 -0300

    Update testa_rpg.py

--- testa_rpg.py
+++ testa_rpg.py
@@ -5,15 +5,33 @@ from lobo import Lobo
 from goblin import Goblin
 
 personagem = Personagem(pontos_de_vida=100, pontos_de_ataque=20, nome="Herói")
-
 goblin = Goblin(pontos_de_vida=50, pontos_de_ataque=10, tipo="Goblin", inteligencia=5)
 
-print("Antes do ataque")
-print(f"{personagem.nome}: {personagem.pontos_de_vida} pontos de vida")
-print(f"{goblin.tipo}: {goblin.pontos_de_vida} pontos de vida")
+print("Início")
+print(f"{personagem.nome}: {personagem.pontos_de_vida} pontos de vida, {personagem.pontos_de_ataque} pontos de ataque")
+print(f"{goblin.tipo}: {goblin.pontos_de_vida} pontos de vida, {goblin.pontos_de_ataque} pontos de ataque")
 
 goblin.atacar(personagem)
+print("Depois do ataque do goblin")
+print(f"{personagem.nome}: {personagem.pontos_de_vida} pontos de vida, {personagem.pontos_de_ataque} pontos de ataque")
+print(f"{goblin.tipo}: {goblin.pontos_de_vida} pontos de vida, {goblin.pontos_de_ataque} pontos de ataque")
 
-print("Depois do ataque")
-print(f"{personagem.nome}: {personagem.pontos_de_vida} pontos de vida")
-print(f"{goblin.tipo}: {goblin.pontos_de_vida} pontos de vida")
+personagem.cura(valor_da_cura=5)
+print("Depois da cura do heroi")
+print(f"{personagem.nome}: {personagem.pontos_de_vida} pontos de vida, {personagem.pontos_de_ataque} pontos de ataque")
+print(f"{goblin.tipo}: {goblin.pontos_de_vida} pontos de vida, {goblin.pontos_de_ataque} pontos de ataque")
+
+personagem.elixir(tipo="vida", valor=20)
+print("Depois do elixir de vida no personagem")
+print(f"{personagem.nome}: {personagem.pontos_de_vida} pontos de vida, {personagem.pontos_de_ataque} pontos de ataque")
+print(f"{goblin.tipo}: {goblin.pontos_de_vida} pontos de vida, {goblin.pontos_de_ataque} pontos de ataque")
+
+goblin.elixir(tipo="ataque", valor=40)
+print("Depois do elixir de ataque no goblin")
+print(f"{personagem.nome}: {personagem.pontos_de_vida} pontos de vida, {personagem.pontos_de_ataque} pontos de ataque")
+print(f"{goblin.tipo}: {goblin.pontos_de_vida} pontos de vida, {goblin.pontos_de_ataque} pontos de ataque")
+
+goblin.atacar(personagem)
+print("Depois do ataque melhorado do goblin")
+print(f"{personagem.nome}: {personagem.pontos_de_vida} pontos de vida, {personagem.pontos_de_ataque} pontos de ataque")
+print(f"{goblin.tipo}: {goblin.pontos_de_vida} pontos de vida, {goblin.pontos_de_ataque} pontos de ataque")
