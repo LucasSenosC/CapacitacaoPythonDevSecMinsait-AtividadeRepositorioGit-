337d8b095cffcd6b508948767cce4561fee5bb4d
Lucas Senos Coutinho <56167804+LucasSenosC@users.noreply.github.com>
2023-03-23 16:03:38 -0300

    Update personagem.py

--- personagem.py
+++ personagem.py
@@ -3,4 +3,4 @@ from servivo import SerVivo
 class Personagem(SerVivo):
     def __init__(self, pontos_de_vida, pontos_de_ataque, nome):
         super().__init__(pontos_de_vida, pontos_de_ataque)
-        self.nome = nome
+        self.nome: str = nome
