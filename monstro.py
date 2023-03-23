6aeea99687609330f6adc316332c4fdc3a6ab6af
Lucas Senos Coutinho <56167804+LucasSenosC@users.noreply.github.com>
2023-03-23 16:02:34 -0300

    Update monstro.py

--- monstro.py
+++ monstro.py
@@ -3,7 +3,9 @@ from servivo import SerVivo
 class Monstro(SerVivo):
     def __init__(self, pontos_de_vida, pontos_de_ataque, tipo):
         super().__init__(pontos_de_vida, pontos_de_ataque)
-        self.tipo = tipo
+        self.tipo: str = tipo
 
+    # Ataque de um monstro a um outro ser vivo
+    # Subtrai os pontos de ataque de um ser vivo A dos pontos de vida de um outro ser vivo B
     def atacar(self, personagem):
         personagem.pontos_de_vida -= self.pontos_de_ataque
